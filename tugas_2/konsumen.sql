632f42adbb6a53153337ac5e73dc404abb771acb
13max579 <31398203+13max579@users.noreply.github.com>
2018-12-09 17:16:32 +0700

    Add files via upload

---
+++ tugas_2/konsumen.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.16  Distrib 10.1.34-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: konsumen
+-- ------------------------------------------------------
+-- Server version	10.1.34-MariaDB-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pesanan`
+--
+
+DROP TABLE IF EXISTS `pesanan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pesanan` (
+  `ID_Pelanggan` int(15) NOT NULL,
+  `Nama` varchar(50) DEFAULT NULL,
+  `Tujuan` varchar(50) DEFAULT NULL,
+  `QTY` int(10) DEFAULT NULL,
+  `Harga` decimal(9,0) DEFAULT NULL,
+  `Tgl_Pesan` datetime DEFAULT NULL,
+  `Kode_Booking` int(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Kode_Booking`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pesanan`
+--
+
+LOCK TABLES `pesanan` WRITE;
+/*!40000 ALTER TABLE `pesanan` DISABLE KEYS */;
+INSERT INTO `pesanan` VALUES (12312,'adef','Candi Borobudur',2,1500000,'2018-12-09 12:34:45',2),(12345,'asdefafe','Pantai Nusa Dua Bali',2,4200000,'2018-12-09 12:56:44',3);
+/*!40000 ALTER TABLE `pesanan` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-12-09 17:07:40
